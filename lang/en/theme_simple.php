101ca1041e9b706d487003e8757abb8a379c2c16
Mary Evans <lazydaisy@visible-expression.co.uk>
2013-04-09 16:18:06 +0100

    MDL-39021 theme_simple: Add simple bootstrap theme to Moodle.

---
+++ lang/en/theme_simple.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Strings for component 'theme_simple', language 'en', branch 'MOODLE_23_STABLE'
+ *
+ * @package   simple theme
+ * @copyright 2013 Bas Brands, www.basbrands.nl
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Simple';
+$string['configtitle'] = 'Simple Bootstrap';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Please add the URL to your new logo only if you want to replace the default logo for this theme.<br />For your information, the default logo is a JPG image 320px (wide) by 75px (high)';
+$string['footnote'] = 'Footnote';
+$string['footnotetxt'] = 'Simple is a modified Moodle Bootstrap theme';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throuout your Moodle site.';
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['choosereadme'] = '
+<div class="clearfix"><div class="theme_screenshot"><h2>Vanguard</h2>
+<img class=img-polaroid src="bootstrap/pix/screenshot.jpg" />
+<h3>Theme Credits</h3>
+<h3>SIMPLE - is a Moodle simple bootstrap theme</h3>
+This theme is based upon the Moodle Bootstrap theme, which was originally created for Moodle with the help of:<br>
+Stuart Lamour, Mark Aberdour, Paul Hibbitts, Mary Evans.
+Authors: Bas Brands, David Scotson, Mary Evans<br>
+Contact: bas@sonsbeekmedia.nl<br>
+Website: <a href="http://www.basbrands.nl">www.basbrands.nl</a><br>
+<h3>Report a bug:</h3>
+<a href="http://tracker.moodle.org">http://tracker.moodle.org</a>
+<h3>More information</h3>';
+
